9d1c7a7cc46dd68f9b6f22e9b7bffcda229344a5
zhouww <zwwko@163.com>
2016-12-07 12:01:13 +0800

    增加avro三种序列化方式

---
+++ src/main/java/avro/Employee.java
@@ -0,0 +1,34 @@
+package avro;
+
+/**
+ * Created by tt on 12/7/16.
+ */
+public class Employee {
+    private int age;
+    private String name;
+    private String ssn;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSsn() {
+        return ssn;
+    }
+
+    public void setSsn(String ssn) {
+        this.ssn = ssn;
+    }
+}
